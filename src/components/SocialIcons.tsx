5612223c359d4e4299f916f0b1a274465078105f
Esha Agarwal <esha.agarwal0212@gmail.com>
2026-03-30 21:13:15 +0200

    Deployment

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -1,12 +1,8 @@
-import {
-  FaGithub,
-  FaInstagram,
-  FaLinkedinIn,
-} from "react-icons/fa6";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 import "./styles/SocialIcons.css";
-import { TbNotes } from "react-icons/tb";
+// import { TbNotes } from "react-icons/tb";
 import { useEffect } from "react";
-import HoverLinks from "./HoverLinks";
+// import HoverLinks from "./HoverLinks";
 
 const SocialIcons = () => {
   useEffect(() => {
