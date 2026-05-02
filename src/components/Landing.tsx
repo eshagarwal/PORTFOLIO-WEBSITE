ea3275af4287abd307019b6ce3d19fc322a3d307
Esha Agarwal <esha.agarwal0212@gmail.com>
2026-05-02 12:26:09 +0200

    Updates Landing component to change job title from Developer to Engineer

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -21,7 +21,7 @@ const Landing = ({ children }: PropsWithChildren) => {
               {/* <div className="landing-h2-2">Problem Solver</div> */}
             </h2>
             <h2>
-              <div className="landing-h2-info">Developer</div>
+              <div className="landing-h2-info">Engineer</div>
               {/* <div className="landing-h2-info-1">Problem Solver</div> */}
             </h2>
           </div>
